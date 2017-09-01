101750554c7637cc12303f2abc8e3e2f07d5ad95
Margeaux Spring <contact@cheapwebmonkey.com>
2017-09-01 17:22:25 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Alexa Skill for the Louisville Mayor's Podcast   
+###  Created by the Civic Data Alliance
+
+This is the Alexa skill for Mayor Greg Fischer's Podcast, using
+nodejs, Lambda and DynamoDB.  
+
+Greg Fischer is the Mayor of Louisville, Kentucky.
---
+++ js/audioAssets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Audio Source - Mayor Greg Fischer's Podcast: http://feeds.soundcloud.com/users/soundcloud:users:326556075/sounds.rss
+var audioData = [
+    {
+        'title' : 'Episode 1: Race, equity, public art review',
+        'url' : 'https://feeds.soundcloud.com/stream/338282587-mayor-greg-fischer-podcast-episode-1-race-equity-public-art-review.mp3'
+    },
+    {
+        'title' : 'Episode 2: Immigration',
+        'url' : 'http://www.podtrac.com/pts/redirect.mp3/feeds.soundcloud.com/stream/340327765-mayor-greg-fischer-podcast-episode-2-immigration.mp3'
+    }
+];
+
+module.exports = audioData;
